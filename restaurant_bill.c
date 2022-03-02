03d932746d578debb37586f505c49c96fd821720
Lindsey Pera <lpera@cpp.edu>
2022-03-01 17:04:35 -0800

    initial outline

---
+++ restaurant_bill.c
@@ -0,0 +1,9 @@
+/*get user input from command line with "int main(int argc, char *argv[])"
+validate input by making sure argc == 2 AND that argv[0] and argv[1] are both floats
+save argv[0] as float variable "tax" and argv[1] as float variable "tip"
+generate random number between 0-4 to choose meal price
+if(randnum = 0) mealprice = 9.95 etc
+calculate tip by multiplying "tip" by meal price
+calculate tax by multiplying "tax" by meal price
+sum tax + tip + mealprice = total bill
+display mealprice, tax, tip, and total*/
